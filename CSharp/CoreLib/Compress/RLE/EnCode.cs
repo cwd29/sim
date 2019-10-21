267db9aa9d70d2757ebf758ab9bb785f7e2a53e0
cwd29 <565178976@qq.com>
2019-10-21 14:35:20 +0800

    # RLE 压缩算法实现初版

--- CSharp/CoreLib/Compress/RLE/EnCode.cs
+++ CSharp/CoreLib/Compress/RLE/EnCode.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreLib.Compress.RLE
 {
-    class EnCode : ICode
+    public class EnCode : ICode
     {
         public const string ERROR_FORMAT = "RLE Encode Error:{0}";
 
@@ -12,17 +13,74 @@ namespace CoreLib.Compress.RLE
             var result = new List<byte>();
             try
             {
-                /**
-                 * 1. 连续2个，看做是不同的
-                 * */
-                var index = 0;
-                var coutIndex = 0;
-                byte b;
-                while (index < src.Length )
-                {
+                // 所有的全部都看做不一样的 
+                var ds = new List<Byte>();
+                var index = 0; // src读取的的下标索引
 
-                    result[coutIndex] += 1;
+                var sameCount = 1;
+                var sameByte = default(byte);
+
+                ds.Add(src[index++]);
+                while (index < src.Length)
+                {
+                    var b = src[index++];
+                    if (ds.Count > 0 && ds.Last() == b) // 不同的话
+                    {
+                        sameCount++;
+                        sameByte = b;
+
+                        if (sameCount >= 3 && ds.Count > 1)
+                        {
+                            ds.RemoveAt(ds.Count() - 1);
+                            result.Add((byte)ds.Count);
+                            result.AddRange(ds);
+                            ds = new List<byte>();
+                            ds.Add(sameByte);
+                        }
+                    }
+                    else
+                    {
+                        if (sameCount >= 3)
+                        {
+                            result.Add((byte)(128 + sameCount));
+                            result.Add(sameByte);
+                            ds = new List<byte>();
+                            sameByte = b;
+                        }
+                        else if (sameCount == 2)
+                        {
+                            ds.Add(sameByte);
+                        }
+                        ds.Add(b);
+                        sameCount = 1;
+                    }
                 }
+                
+                if(sameCount >= 3)
+                {
+                    ds.RemoveAt(ds.Count() - 1);
+                    if(ds.Count() > 0)
+                    {
+                        result.Add((byte)ds.Count);
+                        result.AddRange(ds);
+                    }
+
+                    result.Add((byte)(128 + sameCount));
+                    result.Add(sameByte);
+                }
+                else
+                {
+                    if (sameCount == 2)
+                        ds.Add(sameByte);
+
+                    if (ds.Count() > 0)
+                    {
+                        result.Add((byte)ds.Count);
+                        result.AddRange(ds);
+                    }
+                }
+                
+
             }
             catch (Exception ex)
             {
